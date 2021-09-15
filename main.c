9dda77e2ba812fdee0409e59fbed5dc3bbcdc810
Artem Kostin <artem.1.kostin@gmail.com>
2021-09-14 22:29:00 -0400

    Refactor entry point and add Doxygen comments
    Signed-off-by: Artem Kostin <artem.1.kostin@gmail.com>

--- main.c
+++ main.c
@@ -1,12 +1,40 @@
+/**
+ * @file main.c
+ * @author Artem Kostin (artem.1.kostin@gmail.com)
+ * @brief SmartGardenProject pot controller entry point.
+ * @version 0.1
+ * @date 2021-09-12
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+/**
+ * @brief Initialize all required HW and SW modules.
+ *
+ */
+void bsp_init(void){
+    stdio_init_all();
+}
 
+/**
+ * @brief programm entry point
+ *
+ * @return 0, which is never reached
+ */
 int main()
 {
-    stdio_init_all();
+    bsp_init();
 
     puts("Hello, world!");
+
+    //Mainloop
+    while(1){
+    ;
+    }
 
     return 0;
 }
